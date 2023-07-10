a03d8d5d039270e6a09ec25c7f6e6d06387f1936
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-10 15:08:38 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -516,12 +516,12 @@ with pandas.ExcelWriter(FILE_BUFFER, engine='xlsxwriter') as writer:
     filtered_frame.to_excel(writer, sheet_name='wh_routes_report')
     #writer.close()
 
-    st.download_button(
-        label="Download report as xlsx",
-        data=FILE_BUFFER,
-        file_name=f"route_report_{TODAY}.xlsx",
-        mime="application/vnd.ms-excel"
-    )
+st.download_button(
+    label="Download report as xlsx",
+    data=FILE_BUFFER,
+    file_name=f"route_report_{TODAY}.xlsx",
+    mime="application/vnd.ms-excel"
+)
 snapshot = tracemalloc.take_snapshot()
 top_stats = snapshot.statistics('lineno')
 print("[ Top 20 ]")
