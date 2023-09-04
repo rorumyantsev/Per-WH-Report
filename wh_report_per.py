302296b05747b493a955a162f4b1e2b97c2cfd5c
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-09-04 13:30:01 -0400

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -64,8 +64,8 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
     client_timezone = "America/Lima"
 
     if option == "Monthly":
-        start_ = "2023-07-28"
-        end_ = "2023-08-31"
+        start_ = "2023-08-25"
+        end_ = "2023-09-30"
         today = datetime.datetime.now(timezone(client_timezone))
         date_from_offset = datetime.datetime.fromisoformat(start_).astimezone(
             timezone(client_timezone)) - datetime.timedelta(days=1)
